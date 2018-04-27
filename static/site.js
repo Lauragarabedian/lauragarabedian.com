fbc8d3b4fb594b3bf100f359004cd96c01461c91
Stu Small <stuart.small@threat-x.com>
2018-04-26 22:00:20 -0600

    Intial commit

---
+++ static/site.js
@@ -0,0 +1,19 @@
+function menu_resize() {
+    if (window.innerWidth <= 767) {
+        $("#sidebar").hide();
+        $("#topnav").show();
+        $('#main-doc').addClass('col-12');
+        $('#main-doc').removeClass('col-10');
+    } else {
+        $("#sidebar").show();
+        $("#topnav").hide();
+        $('#main-doc').addClass('col-10');
+        $('#main-doc').removeClass('col-12');
+    }
+}
+
+menu_resize();
+
+$(window).ready(menu_resize);
+
+$(window).resize(menu_resize);
