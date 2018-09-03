d6e9c946564cd8219f7fa6ac6047a86c030d1615
Stu Small <stuart.alan.small@gmail.com>
2018-09-02 19:10:38 -0600

    Small page speed optimizations

--- static/site.js
+++ static/site.js
@@ -1,26 +1,22 @@
-function menu_resize() {
+function on_resize() {
     if (window.innerWidth <= 768) {
-        $("#sidebar").hide();
-        $("#topnav").show();
         $('#main-doc').addClass('main-doc-mobile');
         $('#main-doc').removeClass('main-doc-standard');
 
     } else {
-        $("#sidebar").show();
-        $("#topnav").hide();
         $('#main-doc').addClass('main-doc-standard');
         $('#main-doc').removeClass('main-doc-mobile');
     }
 }
 
-menu_resize();
+on_resize();
 
-$(window).ready(menu_resize);
+$(window).ready(on_resize);
 
-$(window).resize(menu_resize);
+$(window).resize(on_resize);
 
 $( document ).ready(function() {
-    menu_resize();
+    on_resize();
     $('.grid').masonry({
         itemSelector: '.grid-item',
         columnWidth: 300,
